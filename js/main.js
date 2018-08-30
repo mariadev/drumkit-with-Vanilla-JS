e5afd0cb3f5a45ef71f2b394d4a22c10fc8fa2de
Maria Violeta <mariavioleta1417@gmail.com>
2018-08-30 03:56:49 +0200

    add click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 'use strict';
+
 const keys = document.querySelectorAll('.key');
 const arrayColors = ['#5B41C1','#5893E0','#43C2C2', '#58E55C', '#FCF04E','#F25A5F']; 
 let count=0;
 
+function playsoundclick (e){
+  console.log('soy un evento',e.target.innerText);
+  const audioclick = document.querySelector(`audio[data-click="${e.target.innerText}"]`);
+  console.log('soy un evento',audioclick);
+  e.target.classList.add('playing');
+  e.target.classList.add('sound-text');
+  if(!audioclick) return;
+  audioclick.currentTime= 0;
+  audioclick.play();
+  const playColor = document.querySelector('.playing');
+  const soundText = document.querySelector('.sound-text');
+  if (count <= 5){
+    console.log(count);
+    playColor.style.borderColor= arrayColors[count];
+    soundText.style.color= arrayColors[count];
+  }
+  else{
+    count =0;
+    playColor.style.borderColor= arrayColors[count];
+    soundText.style.color= arrayColors[count];
+  }
+  count= count + 1;
+  console.log(count);
+}
 function playsound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -36,4 +61,5 @@ function removeTransition (e){
 }
 
 keys.forEach(key => key.addEventListener ('transitionend',removeTransition));
+keys.forEach(key => key.addEventListener ('click', playsoundclick));
 window.addEventListener('keydown', playsound);
